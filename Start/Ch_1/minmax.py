69ffdb9632d818a147b3da40b376430d35ee16cb
jiteshjkumar <38117535+jiteshjkumar@users.noreply.github.com>
2024-04-02 03:42:14 +0000

    Pending changes exported from your codespace

--- Start/Ch_1/minmax.py
+++ Start/Ch_1/minmax.py
@@ -8,15 +8,32 @@ values = [3.0, 2.5, 5.1, 4.1, 1.8, 1.6, 2.2, 5.7, 6.1]
 strings = ["one", "three", "five", "seven", "eleven", "eighteen"]
 
 
-# TODO: The min() function finds the minimum value
+# # TODO: The min() function finds the minimum value
+# print(f"The minimum value is: {min(values)}")
+# print(f"The minimum value is: {min(strings)}")
 
+# # TODO: The max() function finds the maximum value
+# print(f"The maximum value is: {max(values)}")
+# print(f"The maximum value is: {max(strings)}")
 
-# TODO: The max() function finds the maximum value
-
-
-# TODO: define a custom "key" function to extract a data field
-
+# # TODO: define a custom "key" function to extract a data field
+# print(f"The minimum value is: {min(strings,key=len)}")
+# print(f"The minimum value is: {max(strings,key=len)}")
 
 # TODO: open the data file and load the JSON
-# with open("../../30DayQuakes.json", "r") as datafile:
-#     data = json.load(datafile)
+with open("/workspaces/advanced-python-working-with-data-4312001/30DayQuakes.json", "r") as datafile:
+    data = json.load(datafile)
+
+print(data["metadata"]["title"])
+print("---")
+print(len(data["features"]))
+print("---")
+
+def getmag(dataitem):
+    magnitude = dataitem["properties"]["mag"]
+    if (magnitude is None):
+        magnitude = 0
+    return float(magnitude)
+
+print(min(data["features"],key=getmag))
+print(max(data["features"],key=getmag))
